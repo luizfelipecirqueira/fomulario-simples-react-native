bb1d415e5d0621f260920faa3c17dfb6ed73bfef
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-06-13 01:03:16 -0300

    Iniciando um formulário simples com react-native para prática mobile

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet, TextInput } from "react-native";
 
-export default function App() {
+const UselessTextInput = () => {
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState(null);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text style={styles.titulo}>Formulário Simples com React-Native</Text>
+      </View>
+      <Text style={styles.texto}>Nome:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text style={styles.texto}>Número:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        keyboardType="numeric"
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
+  titulo:{
+    fontSize: 18,
+    margin: 38
+  },
+  texto:{
+    fontSize: 14,
+    margin: 12
+  }
 });
+
+export default UselessTextInput;
